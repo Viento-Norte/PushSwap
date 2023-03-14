3ed2999785f5e4b9f6608b363a78d6bbbb07c300
Viento-Norte <105431581+Viento-Norte@users.noreply.github.com>
2023-03-14 18:22:27 +0100

    implementacion de ft_memcmp
    la utilizo para compara en cadena de caracteres y no como enteros

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: legarcia <legarcia@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/05 18:14:42 by legarcia          #+#    #+#             */
-/*   Updated: 2023/03/05 19:48:49 by legarcia         ###   ########.fr       */
+/*   Updated: 2023/03/14 18:20:32 by legarcia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include<stdio.h>
@@ -46,20 +46,19 @@ while ( count < argc - 1)
 {
 	if( ft_isnotnumbers(&argv[count + 1][0]))
 			return(write(1, "2Error\n", 7));
-	stack[count] = ft_atoi(&argv[count + 1][0]);
 	count++;
 }
 write(1, "point", 5);
 while ( --count > -1)
 {
-	count2 = -1;
-	while (count > ++count2)
+	count2 = count; 
+	while(--count2 > 0 && count2 < count)
 	{
 	  getchar();
-	  if (stack[count] == stack[count2])
-		  return(write(1,"3Error\n", 7));
-  }
-	ft_putnbr_fd(stack[count],1);
+	  if(!ft_memcmp(&argv[count][0], &argv[count2][0], ft_strlen(&argv[count][0])))
+			return(write(1, "3Error\n", 7));
+
+	}
 }
 free(stack);
 return(0);
